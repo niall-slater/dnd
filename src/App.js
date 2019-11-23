55ba27d0ff3e9fb5731a1e72ebcc6f674710ac4b
niall-slater <niallslater@gmail.com>
2019-11-23 16:36:27 +0000

    Initial react commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
+import Stat from './components/Stat';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>DND babey</h3>
+        <div id="stats">
+          <Stat statLabel="STR" statValue="17" subValue="+3"></Stat>
+          <Stat statLabel="DEX" statValue="16" subValue="+2"></Stat>
+          <Stat statLabel="CON" statValue="17" subValue="+3"></Stat>
+          <Stat statLabel="INT" statValue="16" subValue="+2"></Stat>
+          <Stat statLabel="WIS" statValue="8" subValue="-1"></Stat>
+          <Stat statLabel="CHA" statValue="17" subValue="+3"></Stat>
+        </div>
       </header>
     </div>
   );
---
+++ src/components/Stat.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+
+class Stat extends Component {
+  render() {
+    return (
+      <div className="stat">
+        <div className="stat-label">
+          <span>{this.props.statLabel}</span>
+        </div>
+        <div className="stat-value">
+          <span>{this.props.statValue}</span>
+          <sub>{this.props.subValue}</sub>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Stat;
