ac99aa1169998b40c58fba074221a18b54d15c33
niall-slater <niallslater@gmail.com>
2019-12-05 20:47:48 +0000

    dont work :(

--- src/App.js
+++ src/App.js
@@ -1,49 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import Stat from './components/Stat';
-import CharacterBuilder from './components/CharacterBuilder';
+import Character from './components/Character';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h3>DND babey</h3>
-        <div id="stats">
-          {buildStats(CharacterBuilder.generate())}
-        </div>
       </header>
+      <h4>Ability Scores</h4>
+      <Character></Character>
     </div>
   );
 }
-
-function buildStats(stats) {
-  console.log(stats);
-  var statsElements = [];
-
-  for (var statKey in stats) {
-    var subValue = Math.floor((stats[statKey] - 10) / 2);
-    if (subValue >= 0)
-      subValue = "+" + subValue;
-    statsElements.push(<Stat
-        key={statKey}
-        statLabel={statKey.toUpperCase()}
-        statValue={stats[statKey]}
-        subValue={subValue}></Stat>);
-  }
-
-  return statsElements;
-}
-
 export default App;
-
-//MOCKS
-
-var stats = {
-  str: 17,
-  dex: 5,
-  con: 17,
-  int: 12,
-  wis: 8,
-  cha: 9
-};
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,33 +1,67 @@
+import React from 'react';
+import Stat from './Stat';
 import Helpers from './Helpers';
 
-class Character {
-  
-  constructor(stats) {
-    if (!stats) {
-      this.buildRandom();
-    }
+const statNames = [
+  "STR",
+  "DEX",
+  "CON",
+  "INT",
+  "WIS",
+  "CHA"
+];
+
+class Character extends React.Component{
+
+  constructor(props) {
+    super(props);
+    let stats = this.buildRandomStats();
+    this.stats = stats;
+    this.regenerate = this.regenerate.bind(this);
+    this.buildRandomStats = this.buildRandomStats.bind(this);
   }
   
-  buildRandom() {
-    this.stats = {
-      str: 0,
-      dex: 0,
-      con: 0,
-      int: 0,
-      wis: 0,
-      cha: 0,
-    }
-    
-    for (var statKey in this.stats) {
-      var result = 0;
+  regenerate() {
+    console.log("HONK");
+    this.stats = this.buildRandomStats();
+  }
+  
+  buildRandomStats() {
+    let stats = [];
+    for (var statName in statNames) {
+      var statValue = 0;
       var initRolls = [];
       for (var i = 0; i < 4; i++) {
         initRolls.push(Helpers.rollDie(6));
       }
       initRolls = Helpers.removeSmallest(initRolls);
-      result = Helpers.getTotal(initRolls);
-      this.stats[statKey] = result;
+      statValue = Helpers.getTotal(initRolls);
+      var subValue = Math.floor((statValue - 10) / 2);
+      var newStat = {
+        name: statName,
+        value: statValue,
+        subValue: subValue
+      };
+      stats.push(newStat);
     }
+    
+    return stats;
+  }
+
+  render() {
+    return (
+    <div id="stats">
+      {this.stats.map((stat, i) => {return(
+        <Stat
+            key={i}
+            statLabel={stat.name.toUpperCase()}
+            statValue={stat.value}
+            subValue={stat.subValue}>
+        </Stat>
+      )})}
+      <button onClick={this.regenerate}>Give me a character!</button>
+    </div>
+    );
   }
 }
 
